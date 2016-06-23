3228159087219a6ff4665a22106da72ca6d56b8b
Viet Nguyen <viet.nguyen182@gmail.com>
2016-06-23 07:56:16 -0400

    modify requirements

--- README.md
+++ README.md
@@ -3,12 +3,9 @@
 This is a small program to maintain consistent folder structures for
 all my side projects. This is inspired by this [blog post](http://enetdown.org/outgoing/posts/2016/06/19/code-cultivation-to-software-engineering/)
 
-Features (plan):
-
-- Create a new project by asking for: name of the project, the github
-repo, other folders needed (notes, reading, docs, etc.)
+To do: 
 
 - Run a check on the top folder (`projects`) for inconsistent folder
-  structure, missing GitHub repos,  Report if there is any error.
+  structure, missing GitHub repos, and report if there is any error.
 
 
--- app/Main.hs
+++ app/Main.hs
@@ -17,5 +17,18 @@ listOrgs = do
 listProjects :: OrgName -> IO [ProjectName]
 listProjects = undefined 
 
+-- | Check if there is any file (not folder) in the root project folder 
+checkInfantFile :: IO ()
+checkInfantFile = undefined
+-- checkInfantFile = do 
+--   listFilesAndFoldersInRoot <- liftIO $ filename <$> (ls projectsDir)
+--   go listFilesAndFoldersInRoot
+--      where go [] = putStrLn ""
+--            go (x : xs) = case extension x of 
+--                            Just ext -> putStrLn $ x + "is a file and need to be deleted" >> go xs
+--                            Nothing -> go xs
+
+                    
+
 main :: IO ()
 main = listOrgs 
